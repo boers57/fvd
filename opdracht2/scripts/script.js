a33779e4e843f8bf1c4d41b51a206c7a03cff097
boers57 <106560070+boers57@users.noreply.github.com>
2023-06-12 10:33:22 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,17 @@
-// JavaScript Document
-console.log("howdy");
+var jokeContainer = document.getElementById("joke");
+var btn = document.getElementById("btn");
+var url =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+function getJoke() {
+  jokeContainer.classList.add("kleur");
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      // de tekst van de p veranderen in de API tekst
+      jokeContainer.textContent = `${item.joke}`; 
+      btn.textContent = "New Joke";
+    });
+}
+
+btn.addEventListener("click", getJoke);
