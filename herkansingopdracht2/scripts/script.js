976b681476a713ef939b39c9a0c0da6e61075144
boers57 <106560070+boers57@users.noreply.github.com>
2023-06-27 16:20:53 +0200

    Add files via upload

--- herkansingopdracht2/scripts/script.js
+++ herkansingopdracht2/scripts/script.js
@@ -3,12 +3,15 @@ var stopButton = document.getElementById("stop");
 var liList = document.querySelectorAll("li");
 var lightImage = document.getElementById("lights");
 
+var animationPaused = true;
+
 
 function startAnimation () {
   liList.forEach(function (li) {
     li.style.animationPlayState = "running";
   });
   lightImage.style.display = "none";
+  animationPaused = false;
 }
 
 function stopAnimation () {
@@ -16,6 +19,7 @@ function stopAnimation () {
     li.style.animationPlayState = "paused";
   });
   lightImage.style.display ="block";
+  animationPaused = true;
 
   var sound = new Audio("./mp3/tada.mp3");
     sound.play();
@@ -32,19 +36,21 @@ for (let i = 0; i < favButton.length; i++) {
   favButton[i].addEventListener("click", favorietMaken);
 }
 
-function favorietMaken() {  
-  let favAmount = document.querySelector("#like span");
-  let currentAmount = favAmount.innerHTML;
-  currentAmount = parseInt(currentAmount);
-  let newAmount = currentAmount + 1;
-  favAmount.innerHTML = newAmount;
+function favorietMaken() {
+  if (animationPaused) {
+    let favAmount = document.querySelector("#like span");
+    let currentAmount = favAmount.innerHTML;
+    currentAmount = parseInt(currentAmount);
+    let newAmount = currentAmount + 1;
+    favAmount.innerHTML = newAmount;
+    
+    favAmount.classList.add("updated");
+    
+    setTimeout(function(){
+    favAmount.classList.remove("updated");
+    }, 400);  
   
-  favAmount.classList.add("updated");
-  
-  setTimeout(function(){
-  favAmount.classList.remove("updated");
-  }, 400);  
-
-  var sound = new Audio("./mp3/ding.mp3");
-    sound.play();
+    var sound = new Audio("./mp3/ding.mp3");
+      sound.play();
+  }
 }
