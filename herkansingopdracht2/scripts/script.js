6351d763dce41cc8b4bdf9e702e279c0f49b7ba3
boers57 <106560070+boers57@users.noreply.github.com>
2023-06-27 17:10:15 +0200

    Add files via upload

--- herkansingopdracht2/scripts/script.js
+++ herkansingopdracht2/scripts/script.js
@@ -6,22 +6,30 @@ var favButton = document.querySelectorAll("button:first-of-type");
 
 var animationPaused = true;
 
-
+// zorgt dat de animatie start, hij staat eerst op 'pauze'
 function startAnimation () {
+  // voor elke li ingesteld op 'running', staat op 'pauze' in css
   liList.forEach(function (li) {
-    li.style.animationPlayState = "running";
+    li.style.animationPlayState = "running"; 
   });
+  // zorgt ervoor dat de afbeelding niet zichtbaar is als je weer op start klikt
   lightImage.style.display = "none";
+  // geeft aan dat de animatie niet gepauzeerd is
   animationPaused = false;
 }
 
+// zorgt dat de animatie stop
 function stopAnimation () {
+  // voor elke li ingesteld op 'pauze'
   liList.forEach(function (li) {
     li.style.animationPlayState = "paused";
   });
+  // afbeelding is zichtbaar
   lightImage.style.display ="block";
+  // geeft aan dat de animatie wel gepauzeerd is
   animationPaused = true;
 
+  // speelt audio af
   var sound = new Audio("./mp3/tada.mp3");
     sound.play();
 }
@@ -30,13 +38,13 @@ startButton.addEventListener("click", startAnimation);
 stopButton.addEventListener("click", stopAnimation);
 
 // toevoegen aan favorieten
-// Bron: codepen animaties - oefening 3 shopping cart
-
+// code van de bron, weet niet precies hoe alles werkt, wel in de grote lijnen
 for (let i = 0; i < favButton.length; i++) {
   favButton[i].addEventListener("click", favorietMaken);
 }
 
 function favorietMaken() {
+  // kijkt of animationPaused waar is
   if (animationPaused) {
     let favAmount = document.querySelector("#like span");
     let currentAmount = favAmount.innerHTML;
@@ -44,12 +52,15 @@ function favorietMaken() {
     let newAmount = currentAmount + 1;
     favAmount.innerHTML = newAmount;
     
+    // voegt css-class toe
     favAmount.classList.add("updated");
     
+    // na 4 seconden wordt de css-class verwijderd
     setTimeout(function(){
     favAmount.classList.remove("updated");
     }, 400);  
   
+    // speelt audio af
     var sound = new Audio("./mp3/ding.mp3");
       sound.play();
   }
